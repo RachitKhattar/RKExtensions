093f9f187d670b201b59399fe0a43fc29ff63ff8
Rachit Khattar <rachit.khattar@timesinternet.in>
2020-09-07 20:38:45 +0530

    Add Array Extension

---
+++ Extensions/ArrayExtension.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+extension Array where Element: Comparable {
+    func containsSameElements(as other: [Element]) -> Bool {
+        return self.count == other.count && self.sorted() == other.sorted()
+    }
+}
+
+extension Array where Element: Equatable {
+    mutating func appendIfNotContains(_ element: Element) {
+        if !contains(element) {
+            append(element)
+        }
+    }
+    
+    mutating func move(_ element: Element, to newIndex: Index) {
+        if let oldIndex: Int = self.firstIndex(of: element) { self.move(from: oldIndex, to: newIndex) }
+    }
+}
+
+extension Array {
+    mutating func move(from oldIndex: Index, to newIndex: Index) {
+        if oldIndex == newIndex { return }
+        if abs(newIndex - oldIndex) == 1 { return self.swapAt(oldIndex, newIndex) }
+        self.insert(self.remove(at: oldIndex), at: newIndex)
+    }
+}
+
+
+extension Array {
+    func safeValue(at index: Int) -> Element? {
+        if index < self.count {
+            return self[index]
+        } else {
+            return nil
+        }
+    }
+}
